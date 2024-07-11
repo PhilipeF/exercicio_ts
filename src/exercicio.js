d568c64a7a86e5c9f8d07c860f1a6aded4275b10
PhilipeF <philipesferreiraa@gmail.com>
2024-07-11 17:09:03 -0300

    Feat: Exercício_ts

---
+++ src/exercicio.js
@@ -0,0 +1,5 @@
+"use strict";
+const multiplica = (a, b) => a * b;
+console.log(multiplica(7, 80));
+const saudacao = (nome) => `Olá ${nome}`;
+console.log(saudacao('Philipe'));
---
+++ src/exercicio.ts
@@ -0,0 +1,8 @@
+const multiplica = (a: number, b: number) => a * b;
+
+console.log(multiplica(7, 80));
+
+const saudacao = (nome: string) => `Olá ${nome}`
+
+console.log(saudacao('Philipe'))
+
